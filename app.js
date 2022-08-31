51eb8f41d9bd3de31bd291fd2c8cabecda6c1e72
Daniel <vilcadaniel53@gmail.com>
2022-08-30 19:54:12 -0500

    Cambios para calcular y mostrar en header

--- app.js
+++ app.js
@@ -36,3 +36,50 @@ class Egreso extends Dato {
         return this._id;
     }
 }
+const ingresos = [
+    new Ingreso("Salario", 3500.0),
+    new Ingreso("Salario", 100.0),
+];
+const egresos = [
+    new Egreso("Renta", 1000.0),
+    new Egreso("Reparacion pc", 300.0),
+];
+function totalIngresos() {
+    let totalIngresos = 0;
+    for (let i of ingresos) {
+        totalIngresos += i.valor;
+    }
+    return totalIngresos;
+}
+function totalEgresos() {
+    let totalEgresos = 0;
+    for (let i of egresos) {
+        totalEgresos += i.valor;
+    }
+    return totalEgresos;
+}
+function cargarCabecero() {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = `${Math.round(
+        (totalEgresos() / totalIngresos()) * 100
+    )}%`;
+    document.getElementById("presupuesto").innerHTML =
+        formatoMoneda(presupuesto);
+    document.getElementById("porcentaje").innerHTML = porcentajeEgreso;
+    document.getElementById("ingresos").innerHTML = formatoMoneda(
+        totalIngresos()
+    );
+    document.getElementById("egresos").innerHTML = formatoMoneda(
+        totalEgresos()
+    );
+}
+function cargarApp() {
+    cargarCabecero();
+}
+function formatoMoneda(value) {
+    return value.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimunFractionsDigits: 2,
+    });
+}
